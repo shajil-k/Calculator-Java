a9c146346f708781ed6f1bec78c5f64d59ecac8f
shajil-k <shajil7510@gmail.com>
2023-06-09 22:27:40 +0530

    initialised calc project

---
+++ src/calculator/Class1.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Class1 {
+	public static void main(String[] args) {
+		//System.out.println("hello,world");
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter a number");
+		int value = s.nextInt();
+		
+		System.out.println("the value is :" + value);
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hello
+ *
+ */
+module calculator {
+}
